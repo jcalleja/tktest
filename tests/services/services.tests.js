302d5f67082c50949896441079e87f2fad19f338
jcalleja <jcalleja@softstackfactory.org>
2016-02-29 23:58:29 +0000

    service test 1

---
+++ tests/services/services.tests.js
@@ -0,0 +1,18 @@
+describe('Services', function(){
+    var scope;
+    // load the controller's module
+    beforeEach(module('ionic'));
+    beforeEach(module('starter.controllers'));
+    beforeEach(module('RESTConnection'));
+    beforeEach(module('SSFAlerts'));
+    beforeEach(module('SORTService'));
+    //Inject the scope and save it in a variable
+    beforeEach(inject(function($rootScope, service) {
+        scope = $rootScope.$new();
+        $service('sortArray', {$scope: scope});
+    }));
+    // tests start here
+    it('should return an array', function(){
+        expect(a).toEqual("Results");
+    });
+});
---
+++ www/js/SORTService.js
@@ -0,0 +1,10 @@
+angular.module('SORTServicesModule', [])
+    .service('SORTArrayService', function() {
+        var a = [2, 23, 15, 4, 30, 1];
+        
+        service.sortArray = function(a) {
+            var results = [];
+            results = a;
+            return results;
+        };
+    });
