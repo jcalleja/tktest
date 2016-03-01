b1d52c5b33ae77c61e7a4554e3d18eea574aa351
jcalleja <jcalleja@softstackfactory.org>
2016-03-01 00:09:47 +0000

    iunno

--- tests/services/services.tests.js
+++ tests/services/services.tests.js
@@ -2,9 +2,9 @@ describe('Services', function(){
     var scope;
     // load the controller's module
     beforeEach(module('ionic'));
-    beforeEach(module('starter.controllers'));
+  /*  beforeEach(module('starter.controllers'));
     beforeEach(module('RESTConnection'));
-    beforeEach(module('SSFAlerts'));
+    beforeEach(module('SSFAlerts')); */
     beforeEach(module('SORTService'));
     //Inject the scope and save it in a variable
     beforeEach(inject(function($rootScope, service) {
@@ -13,6 +13,6 @@ describe('Services', function(){
     }));
     // tests start here
     it('should return an array', function(){
-        expect(a).toEqual("Results");
+        expect(a).toEqual([2, 23, 15, 4, 30, 1]);
     });
 });
--- www/js/SORTService.js
+++ www/js/SORTService.js
@@ -1,9 +1,9 @@
 angular.module('SORTServicesModule', [])
     .service('SORTArrayService', function() {
         var a = [2, 23, 15, 4, 30, 1];
-        
+        var results = [];
+
         service.sortArray = function(a) {
-            var results = [];
             results = a;
             return results;
         };
